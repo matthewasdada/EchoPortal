7ad533e7866a3de42c0612c8338890ef8c2f9828
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2026-04-06 11:31:19 +0000

    created basic coding for day 1, basic flask applic ation, a home page, a not working login system YET, and finishing it off with a basic CSS

--- EchoPortal-codespace/app.py
+++ EchoPortal-codespace/app.py
@@ -1,2 +1,15 @@
 import os
 from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
