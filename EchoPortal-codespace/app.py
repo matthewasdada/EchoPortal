330da58c2affe497ff2266d80e12628ecc173dd0
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2026-04-07 07:26:43 +0000

    forgot to implement and define redirect to home page and also the url

--- EchoPortal-codespace/app.py
+++ EchoPortal-codespace/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "secretkey"
