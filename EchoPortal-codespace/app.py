2a8370ed784a962db3e3d1dc6dbbd255d3bcaaba
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2026-04-07 07:22:27 +0000

    made a basic authentication system, so that is can check if the user crednetials inputed were validated and helps prevent unauthorised access to the system, making better security

--- EchoPortal-codespace/app.py
+++ EchoPortal-codespace/app.py
@@ -2,15 +2,23 @@ import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
 app.secret_key = "secretkey"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", method=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "1234":
+            return redirect(url_for(home))
+        else:
+            return "Invalid login"
+        
     return render_template("login.html")
 
 if __name__ == "__main__":
