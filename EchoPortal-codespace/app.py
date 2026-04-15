c74a34af0aefae3a4c63bc922c528f8d081acce6
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2026-04-15 10:13:59 +0000

    added a upload config and a route for it, also did some fixing across my htmls, and did some styling and created a folder so upload system doesnt crash

--- EchoPortal-codespace/app.py
+++ EchoPortal-codespace/app.py
@@ -4,6 +4,10 @@ from flask import Flask, request, render_template, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "secretkey_echoportal_mrbrooks"
 
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -35,12 +39,28 @@ def gallery():
         return render_template("gallery.html")
     else:
         return redirect(url_for("login"))
+    
+@app.route("upload", methods=["POST"])
+def upload():
+    if "user" not in session:
+        return redirect(url_for("login"))
+    
+    file = request.files["image"]
+
+    if file:
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        file.save(filepath)
+
+    return redirect(url_for("dashboard"))
+
 
 
 @app.route("/logout")
 def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
